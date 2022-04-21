cb417e7610c059eb206b99b2e151a5d47f44285b
XueMei-L <alu0101225845@ull.edu.es>
2022-04-21 03:27:32 +0200

    testing pass

--- tests/notes.spec.ts
+++ tests/notes.spec.ts
@@ -7,7 +7,6 @@ import {Notes, colours} from '../src/notes';
 console.log = function() {};
 
 describe('Testing for the practice 09', () => {
-
   const notes = Notes.getNotes();
 
   describe('Testing for the class Notes', () => {
@@ -20,64 +19,64 @@ describe('Testing for the practice 09', () => {
     });
   });
 
-  //Add function test
+  // Add function test
   describe('add function test', () => {
     it('notes.addNote() returns New note added! with title: Note01', () => {
       expect(notes.addNote('User1', 'Note01', 'Test01', colours.yellow)).
-      to.be.equal('New User [User1], Sucesfully to add the new note with title: [Note01].');
+          to.be.equal('New User [User1], Sucesfully to add the new note with title: [Note01].');
     });
 
     it('notes.addNote() returns New note added! with title: Note02', () => {
       expect(notes.addNote('User1', 'Note02', 'Test02', colours.yellow)).
-      to.be.equal('Sucesfully to add the new note with title: [Note02].');
+          to.be.equal('Sucesfully to add the new note with title: [Note02].');
     });
 
     it('notes.addNote() returns New note added! with title: Note03', () => {
       expect(notes.addNote('User1', 'Note03', 'Test03', colours.yellow)).
-      to.be.equal('Sucesfully to add the new note with title: [Note03].');
+          to.be.equal('Sucesfully to add the new note with title: [Note03].');
     });
 
     it('notes.addNote() returns Could not add the new note, there is a note has the same title.', () => {
       expect(notes.addNote('User1', 'Note03', 'Test03', colours.yellow)).
-      to.be.equal('Could not add the new note, there is a note has the same title.');
+          to.be.equal('Could not add the new note, there is a note has the same title.');
     });
 
     it('notes.addNote() returns New user with the new note: Note01', () => {
       expect(notes.addNote('User2', 'Note01', 'Test03', colours.yellow)).
-      to.be.equal('New User [User2], Sucesfully to add the new note with title: [Note01].');
+          to.be.equal('New User [User2], Sucesfully to add the new note with title: [Note01].');
     });
   });
 
-  //Remove function test
+  // Remove function test
   describe('remove function test', () => {
     it('notes.removeNote() returns Note removed!', () => {
       expect(notes.removeNote('User1', 'Note03')).
-      to.be.equal('Sucesfully to remove the [Note03] notes.');
+          to.be.equal('Sucesfully to remove the [Note03] notes.');
     });
 
     it('notes.removeNote() returns Error: Title does not exist!', () => {
       expect(notes.removeNote('User1', 'Note_03')).
-      to.be.equal('Error, there is no one note called [Note_03].');
+          to.be.equal('Error, there is no one note called [Note_03].');
     });
 
     it('notes.removeNote() returns Error: User not found!', () => {
       expect(notes.removeNote('u2', 'Note01')).
-      to.be.equal(`Error, User [u2] not found!`);
+          to.be.equal(`Error, User [u2] not found!`);
     });
   });
 
-  //Read a note
+  // Read a note
   describe('readNote function test', () => {
     it('notes.readNote() returns ', () => {
       expect(notes.readNote('User1', 'Note01')).to.be.equal(
-          `- Title: Note01: Body: Test01`
-        );
+          `- Title: Note01: Body: Test01`,
+      );
     });
 
     it('notes.readNote() returns There is no note with the title [n1]', () => {
       expect(notes.readNote('User1', 'n1')).to.be.equal(
-          `There is no note with the title [n1]`
-        );
+          `There is no note with the title [n1]`,
+      );
     });
 
     it('notes.readNote() returns Error, User [u1] not found!', () => {
@@ -85,53 +84,51 @@ describe('Testing for the practice 09', () => {
     });
   });
 
-  //List function test
+  // List function test
   describe('list function test', () => {
     it('notes.listNotes() of User1 returns all notes of User1', () => {
       expect(notes.listNotes('User1')).to.be.equal(
-         `- Note01: Test01\n- Note02: Test02\n`
-        );
+          `- Note01: Test01\n- Note02: Test02\n`,
+      );
     });
 
     it('notes.listNotes() returns Error, User {u1} not found!', () => {
       expect(notes.listNotes('u1')).to.be.equal('Error, User [u1] not found!');
     });
   });
-  
-  //Modify a note
+
+  // Modify a note
   describe('modifyNote function test', () => {
     it('notes.modifyNote() change title ', () => {
       expect(notes.modifyNote('User1', 'Note01', 'title', 'Note_01')).to.be.equal(
-          `Changed title [Note01] to [Note_01]`
-        );
+          `Changed title [Note01] to [Note_01]`,
+      );
     });
 
     it('notes.modifyNote() change body ', () => {
-      expect(notes.modifyNote('User1', 'Note01', 'body', 'This is note01')).to.be.equal(
-          `Changed body [Test01] to [This is note01]`
-        );
+      expect(notes.modifyNote('User1', 'Note_01', 'body', 'This is note01')).to.be.equal(
+          `Changed body [Test01] to [This is note01]`,
+      );
     });
 
     it('notes.modifyNote() change color ', () => {
-      expect(notes.modifyNote('User1', 'Note01', 'color', 'green')).to.be.equal(
-          `Changed color [yellow] to [green]`
-        );
+      expect(notes.modifyNote('User1', 'Note_01', 'color', 'green')).to.be.equal(
+          `Changed color [yellow] to [green]`,
+      );
     });
 
     it('notes.modifyNote() can not change color, title error', () => {
       expect(notes.modifyNote('User1', 'Note', 'color', 'green')).to.be.equal(
-          `Error, there is no one note called [Note].`
-        );
+          `Error, there is no one note called [Note].`,
+      );
     });
 
     it('notes.modifyNote() can not change color, user not found', () => {
       expect(notes.modifyNote('u1', 'Note', 'color', 'green')).to.be.equal(
-          `Error, User [u1] not found!`
-        );
+          `Error, User [u1] not found!`,
+      );
     });
-
   });
-
 });
 
 fs.rmdirSync('./database', {recursive: true});
